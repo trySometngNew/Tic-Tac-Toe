f342c9ee7a16c11f287a6f8a61f259e82d1836dc
Sourabh Lonikar <sourabhlonikar@gmail.com>
2020-06-07 14:52:36 +0530

    Input of player positions in game function

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -5,9 +5,32 @@
 theBoard = {'7':'','8':'','9':'',
             '4':'','5':'','6':'',
             '1':'','2':'','3':''}
+
 def print_board(self, board):
     print(board[7] + '|' + board[8] + '|' + board[9])
     print('-+-+-')
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
     print(board[1] + '|' + board[2] + '|' + board[3])
+
+# This is main function of this game
+def funcname():
+    turn = 'X'
+    count = 0
+
+    for i in range(10):
+        print_board(theBoard)
+        print("It's your tun, " + turn + ". Move to which place?")
+
+        move = input()
+
+        if move < 1 or move > 9:
+            print('Please choose a valid position.')
+        elif theBoard[move] == '':
+            theBoard[move] = turn
+        else:
+            print('That position is already filled. Please choose another position.')
+            continue
+
+
+    
