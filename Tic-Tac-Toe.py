ab51bff1a6695448a00e26697aff2d5849b5f170
Sourabh Lonikar <sourabhlonikar@gmail.com>
2020-06-07 14:42:30 +0530

    print_board function

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,2 +1,13 @@
 # Tic-Tac-Toe Game in python
 # Source code at https://medium.com/byte-tales/the-classic-tic-tac-toe-game-in-python-3-1427c68b8874
+
+
+theBoard = {'7':'','8':'','9':'',
+            '4':'','5':'','6':'',
+            '1':'','2':'','3':''}
+def print_board(self, board):
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[1] + '|' + board[2] + '|' + board[3])
